d82b80ee50c9d57ba5ddf2c22743380be78ef63c
suhas-malhotra <suhas-int@frigga.app>
2023-12-08 16:15:50 +0530

    added static images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/suhas-malhotra/bookings/pkg/config"
 	"github.com/suhas-malhotra/bookings/pkg/handlers"
-	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -24,6 +25,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
